4ab3fbd69556a64df6bdb21ca881123599a8e0a9
Ravch <110690059+Ravi-chandera@users.noreply.github.com>
2024-05-12 21:05:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import google.generativeai as genai
+
+def main():
+    st.title("Question Answering App")
+    
+    user_profile = st.text_area("Enter your profile:", height=100)
+            # Process the user's question and generate the response        
+        # Display the response using st.markdown()
+    user_goal = st.text_area("Enter your goal:", height=100)
+    
+    if st.button("Get Answer"):
+        # Process the user's question and generate the response
+        response = generate_response(user_profile,user_goal)
+        
+        # Display the response using st.markdown()
+        st.markdown(response)
+
+def generate_response(user_profile,user_goal):
+    prompt_parts = [
+  f" You are a talented and experienced {user_profile}. returns a list of questions for a survey that will help reach the goal. Goal: {user_goal}",
+]
+
+    GOOGLE_API_KEY = "AIzaSyCPtSR4Bk7sSIvoOEPiRywsYt2UkJ0Y5oc"
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt_parts)
+    
+    return response
+
+if __name__ == "__main__":
+    main()
